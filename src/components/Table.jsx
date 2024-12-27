a4847a703a6250bad5fe5df80d88cf103b78a34b
aarshpatel1 <aarsh18patel@gmail.com>
2024-12-27 11:40:33 +0530

    fix problem when there is no user data

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Table(users) {
   return (
     <>
-      <div className="container">
+      <div className="container pb-5">
         <h1 className="text-center mb-5">User Data</h1>
         <table className="table table-striped table-hover border border-secondary-subtle p-5">
           <thead>
@@ -15,14 +15,20 @@ function Table(users) {
             </tr>
           </thead>
           <tbody>
-            {users.userData.map((user, index) => (
-              <tr key={index}>
-                <th scope="row">{index + 1}</th>
-                <td>{user.username}</td>
-                <td>{user.email}</td>
-                <td>{user.password}</td>
+            {users.userData.length > 0 ? (
+              users.userData.map((user, index) => (
+                <tr key={index}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>{user.password}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <th colSpan={4} className="text-center">There is no user data.</th>
               </tr>
-            ))}
+            )}
           </tbody>
         </table>
       </div>
